9eb6bb04b486ab34c9da49c7b8d5d1736b8aeac8
Sam Snarr <sssnarr1@gmail.com>
2020-07-14 21:42:18 -0400

    implemented url parsing for OAuth redirect and http get to Fitbit's API -- success

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DataComponent } from './data/data.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'callback', component: DataComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataComponent } from './data/data.component';
+import { HttpClientModule }    from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
